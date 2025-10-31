34d01f2a011295d81c45a1f6e40c624c492293d8
Thallis <thallis.ferreira2019@gmail.com>
2025-10-31 02:11:21 -0300

    Create Notification Service

---
+++ src/main/java/paynow/example/paynow/service/NotificationService.java
@@ -0,0 +1,29 @@
+package paynow.example.paynow.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import paynow.example.paynow.domain.user.User;
+import paynow.example.paynow.dto.NotificationDTO;
+
+@Service
+public class NotificationService {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public void sendNotification(User user, String message) throws Exception {
+       String email = user.getEmail();
+
+       NotificationDTO notificationRequest = new NotificationDTO(email, message);
+
+       ResponseEntity<String> notificationResponse = restTemplate.postForEntity(
+               "https://util.devi.tools/api/v2/notify", notificationRequest, String.class);
+
+       if(!(notificationResponse.getStatusCode() == HttpStatus.OK)){
+           throw new Exception("Notification Service out of service");
+       }
+    }
+}
